9b705ac6141416aa6e3201fe83313be6e2ccc11d
Andrew Nelson <anelson@cpan.org>
2011-07-08 14:31:54 -0500

    add subject to messages

--- lib/Catalyst/Plugin/MessageStack.pm
+++ lib/Catalyst/Plugin/MessageStack.pm
@@ -155,6 +155,7 @@ sub message {
             $s->{level}   = $message->{type} || $default;
             $s->{id}      = $message->{message};
             $s->{scope}   = $message->{scope} || 'global';
+            $s->{subject} = $message->{subject};
         } else {
             $s->{level}   = $default;
             $s->{id}      = $message;
